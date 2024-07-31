6d9ce1f58fa1e455f78a2306210363c13fc0e172
JulienMorel76350 <Julien.morel76350@gmail.com>
2024-08-01 00:21:23 +0200

    👔 Adding logic for new vacation

--- 03–Architecture MVC–Symfony/vacation_app/src/Controller/VacationController.php
+++ 03–Architecture MVC–Symfony/vacation_app/src/Controller/VacationController.php
@@ -2,17 +2,47 @@
 
 namespace App\Controller;
 
+use App\Entity\Vacation;
+use App\Form\VacationType;
+use App\Repository\VacationRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/vacations')]
 class VacationController extends AbstractController
 {
-    #[Route('/vacation', name: 'app_vacation')]
-    public function index(): Response
+    #[Route('/', name: 'vacation_index', methods: ['GET'])]
+    public function index(VacationRepository $vacationRepository): Response
     {
+        $user = $this->getUser();
         return $this->render('vacation/index.html.twig', [
-            'controller_name' => 'VacationController',
+            'vacations' => $vacationRepository->findBy(['user' => $user]),
         ]);
     }
+
+    #[Route('/new', name: 'vacation_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $vacation = new Vacation();
+        $form = $this->createForm(VacationType::class, $vacation);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $vacation->setUser($this->getUser());
+            $vacation->setStatus('Requested');
+            $entityManager->persist($vacation);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('vacation_index');
+        }
+
+        return $this->render('vacation/new.html.twig', [
+            'vacation' => $vacation,
+            'form' => $form->createView(),
+        ]);
+    }
+
 }
