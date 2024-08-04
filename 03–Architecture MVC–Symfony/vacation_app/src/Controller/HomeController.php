2452b0f309723df7ccafa861884492087cada71a
JulienMorel76350 <Julien.morel76350@gmail.com>
2024-08-04 19:14:42 +0200

    👔 Adding redirection if not logged

--- 03–Architecture MVC–Symfony/vacation_app/src/Controller/HomeController.php
+++ 03–Architecture MVC–Symfony/vacation_app/src/Controller/HomeController.php
@@ -11,6 +11,12 @@ class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
+        // Check if the user is authenticated
+        if (!$this->getUser()) {
+            // Redirect to login page if not authenticated
+            return $this->redirectToRoute('app_login');
+        }
+        
         return $this->render('home/index.html.twig');
     }
 }
--- 03–Architecture MVC–Symfony/vacation_app/src/Controller/VacationController.php
+++ 03–Architecture MVC–Symfony/vacation_app/src/Controller/VacationController.php
@@ -18,6 +18,13 @@ class VacationController extends AbstractController
     #[Route('/', name: 'vacation_index', methods: ['GET'])]
     public function index(VacationRepository $vacationRepository): Response
     {
+        // Check if the user is authenticated
+        if (!$this->getUser()) {
+            $this->addFlash('error', 'You need to be connected to see your vacation.');
+            // Redirect to login page if not authenticated
+            return $this->redirectToRoute('app_login');
+        }
+
         $user = $this->getUser();
         return $this->render('vacation/index.html.twig', [
             'vacations' => $vacationRepository->findBy(['user' => $user]),
@@ -27,6 +34,13 @@ class VacationController extends AbstractController
     #[Route('/new', name: 'vacation_new', methods: ['GET', 'POST'])]
     public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
+        // Check if the user is authenticated
+        if (!$this->getUser()) {
+            $this->addFlash('error', 'You need to be connected to create a new vacation.');
+            // Redirect to login page if not authenticated
+            return $this->redirectToRoute('app_login');
+        }
+
         $vacation = new Vacation();
         $form = $this->createForm(VacationType::class, $vacation);
         $form->handleRequest($request);
@@ -49,6 +63,13 @@ class VacationController extends AbstractController
     #[Route('/{id}/edit', name: 'vacation_edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, Vacation $vacation, EntityManagerInterface $entityManager): Response
     {
+        // Check if the user is authenticated
+        if (!$this->getUser()) {
+            $this->addFlash('error', 'You need to be connected to edit a vacation.');
+            // Redirect to login page if not authenticated
+            return $this->redirectToRoute('app_login');
+        }
+
         if ($vacation->getUser() !== $this->getUser()) {
             $this->addFlash('error', 'You cannot edit this vacation.');
             return $this->redirectToRoute('vacation_index');
@@ -81,6 +102,13 @@ class VacationController extends AbstractController
     #[Route('/{id}', name: 'vacation_delete', methods: ['POST'])]
     public function delete(Request $request, Vacation $vacation, EntityManagerInterface $entityManager): Response
     {
+        // Check if the user is authenticated
+        if (!$this->getUser()) {
+            $this->addFlash('error', 'You need to be connected to delete a vacation.');
+            // Redirect to login page if not authenticated
+            return $this->redirectToRoute('app_login');
+        }
+
         if ($vacation->getUser() !== $this->getUser()) {
             $this->addFlash('error', 'You cannot delete this vacation.');
             return $this->redirectToRoute('vacation_index');
@@ -97,6 +125,13 @@ class VacationController extends AbstractController
     #[Route('/{id}/approve', name: 'vacation_approve', methods: ['POST'])]
     public function approve(Request $request, Vacation $vacation, EntityManagerInterface $entityManager): Response
     {
+        // Check if the user is authenticated
+        if (!$this->getUser()) {
+            $this->addFlash('error', 'You need to be connected to approve a vacation.');
+            // Redirect to login page if not authenticated
+            return $this->redirectToRoute('app_login');
+        }
+
         if ($vacation->getUser() !== $this->getUser()) {
             $this->addFlash('error', 'You cannot approve this vacation.');
             return $this->redirectToRoute('vacation_index');
